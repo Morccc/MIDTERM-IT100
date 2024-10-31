6af6d2fb290a2c09fbe269d6f0faff056ba5e3f4
Renshmackle <renzsabrido1@gmail.com>
2024-10-31 22:22:51 +0800

    first commit not final

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+
+import 'aos/dist/aos.css'
+
+createApp(App).mount('#app')
